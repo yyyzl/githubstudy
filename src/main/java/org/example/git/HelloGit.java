8867b2ea907397bf7a58b4370c6752a560c49877
e <1045535878@qq.com>
2021-01-22 15:53:28 +0800

    第一次提交

---
+++ src/main/java/org/example/git/HelloGit.java
@@ -0,0 +1,11 @@
+package org.example.git;
+
+/**
+ * @Author: yyyzl
+ * @Date: 2021/1/22
+ **/
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("第一次到暂存区和提交");
+    }
+}
